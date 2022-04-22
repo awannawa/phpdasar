ac61d20677d294449a0338dad07515dec411f4d1
awannawam <kurniawanofficiall@gmail.com>
2022-04-22 11:04:53 +0700

    uploaded ke github

---
+++ pertemuan2/index.php
@@ -0,0 +1,50 @@
+<?php
+// Pertemuan 2 - PHP Dasar
+// Sintaks PHP - Eps 4 WPU
+
+// Standar Output
+// echo, print
+// print_r
+// var_dump
+
+// Penulisan sintaks PHP
+// 1. PHP di dalam HTML
+// 2. HTML di dalam PHP
+// variabel dan tipe data
+
+// variable
+// tidak bolehdiawali dengan angka tapi boleh menggunakan angka
+// operator
+// aritmatika
+// + - * / %
+
+// operator penggabung string/ concat
+// . 
+// $nama_depan = "awan";
+// $nama_belakang = "nawa";
+
+// echo $nama_depan . " " . $nama_belakang;
+
+// Assigment
+// = += -= *= /= %= .=
+// $x = 1;
+// $x -= 5;
+// echo $x;
+
+// Perbandungan
+// < > <= >= == !=
+
+// var_dump(1 == "1");
+
+// identitas
+// === !==
+
+// var_dump(1 === "1");
+
+// Logika
+// && || !
+
+// $x = 10;
+// var_dump($x < 20 && $x % 2 == 0);
+
+?>
