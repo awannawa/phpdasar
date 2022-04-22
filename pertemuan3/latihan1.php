491b6da6cf30c9b0831599d96afa492da6ccb6e0
awannawam <kurniawanofficiall@gmail.com>
2022-04-22 11:06:43 +0700

    update di samaain sama pembelajaran terakhir

--- pertemuan3/latihan1.php
+++ pertemuan3/latihan1.php
@@ -42,15 +42,15 @@ do{
 <body>
 
 <table border="1" cellpadding="10" cellspacing="0">
-    <?php
-        for ($i = 1; $i <= 3; $i++){
-            echo "<tr>";
-            for( $j = 1; $j <=5; $j++){
-                echo "<td>$i,$j</td>";
-            }
-            echo "</tr>";
-        }
-    ?>
+    <?php for ($i = 1; $i <=3; $i++) : ?>
+        <tr>
+            <?php for ($j = 1; $j <=5; $j++) : ?>
+                <td><?php echo "$i,$j"; ?>
+                <!-- <td><?= "$i,$j";?></td> bisa pilih salah satu ini atau atas ^^-->
+                <?php endfor; ?>
+        </tr>
+    <?php endfor; ?>    
+
 </table>
 
 
